713649db09575166d71763254a7f4bfc102b57f5
SunMicheal <SSun>
2015-01-30 11:26:03 +0800

    占位参数与类型约束

--- SwiftDemo/Player.swift
+++ SwiftDemo/Player.swift
@@ -79,13 +79,26 @@ func swapTwoValues<T>(inout a:T , inout b:T){    //泛型函数 <T>占位类型 
     b = temporary
 }
 
-struct IntStack {           //将此例中的int 换成T 就是泛型版本 栈
+struct IntStack : Container{           //将此例中的int 换成T 就是泛型版本 栈
+    // IntStack 的原始实现
     var items = [Int]()
     mutating func push(item: Int){
         items.append(item)
     }
     mutating func pop() ->Int {
         return items.removeLast()
+    }
+    
+    // 遵循Container协议的实现
+    typealias ItemType = Int
+    mutating func append(item: ItemType) {
+        self.push(item)
+    }
+     var count: Int {
+        return items.count
+    }
+    subscript(i :Int) ->Int{
+        return items[i]
     }
 }
 
@@ -97,5 +110,29 @@ func findStringIndex<T :Equatable>(array: [T] ,valueToFind: T) -> Int?{  //T 要
     }
     return nil
 }
+
+
+protocol Container {
+    typealias ItemType     //声明关联类型
+    mutating func append(item :ItemType)
+     var count: Int {get}
+    subscript(i: Int) -> ItemType{
+        get
+    }
+}
+
+// 类型约束
+func allItemsMatch <C1: Container ,C2: Container where C1.ItemType == C2.ItemType ,C1.ItemType: Equatable> (someContainer: C1 , anotherContainer: C2) ->Bool {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    for i in 0 ..< someContainer.count {
+        if someContainer[i] != anotherContainer[i]{
+            return false
+        }
+    }
+    return true
+}
+//C1和C2是容器的两个占位类型参数 遵循Container协议，C1的itemType 遵循Equatable协议，<> 表示占位参数类型
 
 
